b3789e80c49f3f0927c836a20647813846c70ee4
NathanCazatti <nathan.moreira.rosa@escola.pr.gov.br>
2025-08-11 12:25:29 -0300

    botao voltar

--- pages/Nathan/script.js
+++ pages/Nathan/script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', async () => {
   const container = document.getElementById('pokemonContainer');
   
   try {
-      const ghostTypeResponse = await fetch('https://pokeapi.co/api/v2/type/8/');
+      const ghostTypeResponse = await fetch('https://pokeapi.co/api/v2/type/2/');
       const ghostTypeData = await ghostTypeResponse.json();
       
       const ghostPokemon = ghostTypeData.pokemon.slice(0, 92);
